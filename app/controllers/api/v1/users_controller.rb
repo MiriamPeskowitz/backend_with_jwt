96dc64d98ba0527b4b8516417ad8edd0ad45caa8
MiriamPeskowitz <miriam5000@gmail.com>
2020-03-29 22:12:53 -0400

    finished auth template

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::UsersController < ApplicationController
+	skip_before_action :authorized, only: [:create]
+
 	def index
 		users = User.all 
 		render json: users
 	end 
 
 	def create 
-		user = User.create(user_params)
-		if user.valid?
-			render json: {user: UserSerializer.new(@user)}, status: 201
+		@user = User.create(user_params)
+		if @user.valid?
+			@token = encode_token(user_id: @user.id)
+			render json: {user: UserSerializer.new(@user)}, jwt: @toekn, status: :created
 		else
 			render json: {error: 'failed to create user'}, status: :not_acceptable
 		end 
